c1cb4858b62281351f691c1ccd9ce4f700f1fdb6
Adam Wangrat <amvangradt@gmail.com>
2016-04-29 17:39:38 +0200

    Próby wprowadzenia edycji

--- Front-end/Front-end/Scripts/eventsHandler.js
+++ Front-end/Front-end/Scripts/eventsHandler.js
@@ -1,6 +1,4 @@
-﻿var doses = [
-    
-]
+﻿var doses = []
 
 var days_of_week = [
     "Poniedziałek",
@@ -46,7 +44,8 @@ function load_doses() {
                     '<p><b>Dawka: </b>' + doses[i].dose + '</p>' +
                     '<p><b>Częstotliwość: </b>' + freq + '</p>' +
                     '<button type="button" class="btn btn-danger" onclick="$(\'#' + i + '\').remove(); doses.splice(' + i + ', 1);">Usuń</button>' +
-                    '<button data-toggle="modal" data-target="#form" type="button" class="btn" onclick="duplicate(' + i + ')">Duplikuj</button>' +
+                    '<button data-toggle="modal" data-target="#form" type="button" class="btn btn-primary" onclick="edit(' + i + ')">Edytuj</button>' +
+                    '<button data-toggle="modal" data-target="#form" type="button" class="btn btn-success" onclick="duplicate(' + i + ')">Duplikuj</button>' +
                 '</div>' +
             '</a>'
          );
@@ -154,6 +153,97 @@ function duplicate(i) {
     document.forms[0].freq_opt5.checked = dose.freq_opts[4];
     document.forms[0].freq_opt6.checked = dose.freq_opts[5];
     document.forms[0].freq_opt7.checked = dose.freq_opts[6];
+}
+
+function edit(i) {
+    $(document).ready();
+
+    var dose = doses[i];
+
+    document.forms[0].drug_name.value = dose.drug_name;
+    document.forms[0].dose.value = dose.dose;
+    document.forms[0].what_time.value = dose.what_time;
+    document.forms[0].how_long.value = dose.how_long;
+    document.forms[0].start_day.value = dose.start_day;
+    document.forms[0].end_day.value = dose.end_day;
+    document.forms[0].comment.value = dose.comment;
+
+    switch (dose.freq) {
+        case "1":
+            document.getElementsByName("freq")[0].checked = true;
+            break;
+        case "2":
+            document.getElementsByName("freq")[1].checked = true;
+            break;
+        case "3":
+            document.getElementsByName("freq")[2].checked = true;
+            break;
+        case "4":
+            document.getElementsByName("freq")[3].checked = true;
+            break;
+    }
+
+    document.forms[0].freq_opt.value = dose.freq_opt;
+
+    $(document).ready();
+
+    document.forms[0].freq_opt1.checked = dose.freq_opts[0];
+    document.forms[0].freq_opt2.checked = dose.freq_opts[1];
+    document.forms[0].freq_opt3.checked = dose.freq_opts[2];
+    document.forms[0].freq_opt4.checked = dose.freq_opts[3];
+    document.forms[0].freq_opt5.checked = dose.freq_opts[4];
+    document.forms[0].freq_opt6.checked = dose.freq_opts[5];
+    document.forms[0].freq_opt7.checked = dose.freq_opts[6];
+
+    $("#submit").hide();
+    
+    $("#form1").prepend(
+        '<button id="submit" type="submit" class="btn btn-default col-lg-5" onclick="update(' + i + ')">Zapisz</button>'
+    );
+}
+
+function update(i) {
+
+    event.preventDefault();
+
+    var form = document.forms[0];
+
+    var freq1 = document.getElementsByName("freq");
+    
+    var cos;
+
+    for (var i = 0; i < freq1.length; i++) {
+        if (freq1[i].checked == true) {
+            cos = freq1[i].value;
+        }
+    }
+
+    doses[i] = {
+        drug_name: form.drug_name.value,
+        dose: form.dose.value,
+        what_time: form.what_time.value,
+        how_long: form.how_long.value,
+        start_day: form.start_day.value,
+        end_day: form.end_day.value,
+        comment: form.comment.value,
+        freq: cos,
+        freq_opt: form.freq_opt.value,
+        freq_opts: [
+            form.freq_opt1.checked,
+            form.freq_opt2.checked,
+            form.freq_opt3.checked,
+            form.freq_opt4.checked,
+            form.freq_opt5.checked,
+            form.freq_opt6.checked,
+            form.freq_opt7.checked,
+        ]
+   }
+
+   $('#form').modal('hide');
+
+   form.reset();
+
+   $("#submit").show();
 }
 
 
